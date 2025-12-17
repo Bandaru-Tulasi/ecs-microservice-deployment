b90e81146047d641b5b07050a7e9ca02f0621d66
Bandaru Tulasi <51985036+Bandaru-Tulasi@users.noreply.github.com>
2025-12-17 17:06:22 -0600

    Updated with little changes app.py

--- app.py
+++ app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on ECS Project!"
+    return "Hello from ECS Automated CI/CD Pipeline!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+

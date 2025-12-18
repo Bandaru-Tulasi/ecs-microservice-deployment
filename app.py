d5748d467d20ec7a1b86805cf94fec079b338559
Bandaru Tulasi <your_github_email_here>
2025-12-17 18:54:51 -0600

    Updated app message to test CI/CD pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from ECS CI/CD Pipeline — Version 2 🚀"
+    return "Hellooo from ECS CI/CD Pipeline  🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)

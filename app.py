ea8cf22b8b69481ab76e690f5ba1edfe8a7e67fb
Bandaru Tulasi <51985036+Bandaru-Tulasi@users.noreply.github.com>
2025-12-21 13:19:27 -0600

    Update app.py

--- app.py
+++ app.py
@@ -4,10 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from VERSION deployed via CI/CD!"
+    return "Hello from NEW VERSION v3!!!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
+
 
 
 
